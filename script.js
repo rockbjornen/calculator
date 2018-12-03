b3db89ae758c6adb3c3aba86e3d09d13c988347d
björn <bjorn.lundkvist@netlight.com>
2018-12-03 09:05:07 +0100

    Added initial handling separation for clicks on number and operator buttons

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const buttons = document.querySelectorAll('.btn');
+let displayedValue = document.querySelector('.display-value');
+
+let currentValue = 0;
+let operator = null;
+const operators = { "+": "+", "-": "-", "x": "*" }
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function (e) {
         const clickedValue = e.target.innerText;
         const num = parseFloat(clickedValue);
-        if(!isNaN(num)){
+        if (!isNaN(num)) {
             //Handle number clicks here
             console.log(num);
+            displayedValue.innerText = num;
         }
         else {
             //Handle operator clicks here
-            console.log(clickedValue);
+            operator = clickedValue;
+            if (operator && operator !== "=") {
+                console.log(operators[operator]);
+            }
         }
     }
 }
